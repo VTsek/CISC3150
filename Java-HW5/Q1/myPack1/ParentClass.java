f00773e936489c5d39f282f0813bb6a1d5b20571
Vitaliy_Java <Vitaliy.Tsek@gmail.com>
2015-11-11 17:51:37 -0500

    Changes

--- Java-HW5/Q1/myPack1/ParentClass.java
+++ Java-HW5/Q1/myPack1/ParentClass.java
@@ -4,6 +4,7 @@ public class ParentClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		protected int protInt =7l;
 		System.out.println("Hi");
 	}
 
--- Java-HW5/Q1/myPack2/ChildClass.java
+++ Java-HW5/Q1/myPack2/ChildClass.java
@@ -1,5 +1,12 @@
 package myPack2;
 
-public class ChildClass {
+import myPack1.ParentClass;
 
+public class ChildClass extends ParentClass {
+
+	public static void main(String [] args){
+		ChildClass childObj = new ChildClass();
+		
+		System.out.println(childObj.protInt);
+	}
 }
