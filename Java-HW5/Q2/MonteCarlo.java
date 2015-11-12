44a32d70dfcbca302a80d78075f9f41532a67ec9
Vitaliy_Java <Vitaliy.Tsek@gmail.com>
2015-11-12 00:02:22 -0500

    Update

---
+++ Java-HW5/Q2/MonteCarlo.java
@@ -0,0 +1,9 @@
+
+public class MonteCarlo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This code got overwritten while learning how to push from Eclipse to Github, I'm working on recovery and uploading it");
+	}
+
+}
